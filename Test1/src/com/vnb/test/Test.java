278e20971237bd8e1155131ed11f773fc06ba80b
2016wanghongjian <874798940@qq.com>
2018-01-05 15:14:11 +0800

    这是第一次新建文件并用master分支

---
+++ Test1/src/com/vnb/test/Test.java
@@ -0,0 +1,17 @@
+package com.vnb.test;
+
+/**
+ * @时间:2018年1月5日 下午3:01:40
+ * @Description: TODO
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("这是第一次的分支master");
+	}
+
+}
