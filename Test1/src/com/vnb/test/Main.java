9e6530f07533ffe07d84657dba544b4f73d8b15b
2016wanghongjian <874798940@qq.com>
2018-01-05 14:24:40 +0800

    Test1项目创建

---
+++ Test1/src/com/vnb/test/Main.java
@@ -0,0 +1,17 @@
+package com.vnb.test;
+
+/**
+ * @时间:2018年1月5日 下午2:22:20
+ * @Description: TODO
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("测试1....");
+	}
+
+}
